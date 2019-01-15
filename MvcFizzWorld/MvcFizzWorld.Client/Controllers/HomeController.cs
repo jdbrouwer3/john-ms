99c543503c1bc7f0cc6dbb40fa01666046a3db74
John <jdbrouwer3@gmail.com>
2019-01-15 00:11:49 -0500

    updating MvcFizzWorld

--- MvcFizzWorld/MvcFizzWorld.Client/Controllers/HomeController.cs
+++ MvcFizzWorld/MvcFizzWorld.Client/Controllers/HomeController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MvcFizzWorld.Client.Models;
 
 namespace MvcFizzWorld.Client.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            for (var i = 1; i <= 50; i++)
+            {
+                ViewData[i.ToString()] = Invite.Categorize(i);
+            }
             return View();
         }
     }
--- MvcFizzWorld/MvcFizzWorld.Client/Models/Invite.cs
+++ MvcFizzWorld/MvcFizzWorld.Client/Models/Invite.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace MvcFizzWorld.Client.Models
 {
-    public class Invite
+    public static class Invite
     {
+        public static string Categorize(int number)
+        {
+            string category = "";
+            if (number % 3 == 0 && number % 5 == 0)
+            { category = "FizzBuzz"; }
+            else if (number % 3 == 0)
+            { category = "Fizz"; }
+            else if (number % 5 == 0)
+            { category = "Buzz"; }
+
+            return category;
+        }
     }
 }

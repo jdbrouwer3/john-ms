66c029dd323f226ff2bbe9b9d28a01b568882791
John <jdbrouwer3@gmail.com>
2019-01-22 23:45:52 -0500

    creating mvc practice

---
+++ MvcPractice/MvcPractice.Client/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using MvcPractice.Client.Models;
+
+namespace MvcPractice.Client.Controllers
+{
+    [Route("[Controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var m = new Menu();
+            //{
+            //    Message = "Thanks for choosing JB's!"
+            //};
+
+            return View("Index", m);
+        }
+            
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Post(Menu menu)
+        {
+            if (menu.Location > 0)
+            {
+                return View("Processed");
+            }
+
+            return View("Unsuccessful");
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ MvcPractice/MvcPractice.Client/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPractice.Client.Models
+{
+    public class Menu
+    {
+        public int Location { get; set; }
+        public int Size { get; set; }
+        public int Crust { get; set; }
+        public int Pizza { get; set; }
+    }
+}

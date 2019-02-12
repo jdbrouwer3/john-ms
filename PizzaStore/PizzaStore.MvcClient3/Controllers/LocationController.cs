4e2968d49199ec5933bcf6855de70856a6dba48e
John <jdbrouwer3@gmail.com>
2019-02-11 20:04:51 -0500

    practicing with a new mvc

---
+++ PizzaStore/PizzaStore.MvcClient3/Controllers/LocationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaStore.MvcClient3.Models;
+
+namespace PizzaStore.MvcClient3.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Pizza(LocationViewModel locationViewModel)
+        {
+            //ViewBag.Name = "florida";
+            TempData.Name = "Florida";
+            //ViewBag.Name = locationViewModel.Name;
+            ViewBag.Name = LocationName;
+
+            return View();
+        }
+    }
+}
--- PizzaStore/PizzaStore.MvcClient3/Models/LocationViewModel.cs
+++ PizzaStore/PizzaStore.MvcClient3/Models/LocationViewModel.cs
@@ -4,11 +4,14 @@ using System.Text;
 using PizzaStore.Domain.Models;
 using PizzaStore.Data.Helpers;
 
-namespace PizzaStore.CliClient.ViewModels
+namespace PizzaStore.MvcClient3.Models
 {
     public class LocationViewModel
     {
-        public List<Location> GetLocations() 
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+
+        public List<Location> GetLocations()
         {
             return LocationHelper.GetLocations();
         }
